3f56ea2f6954ee775ad9a4fc933327ccbc29c7f9
shellbots-team <wsr.shell.bots@gmail.com>
2020-11-25 14:41:48 -0600

    Created Shorty branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blue/Line/BlueInterLineCenter.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Blue.Line;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.BaseAutonomous;
+
+/**
+ * Created by shell on 9/28/2020.
+ */
+
+@Autonomous
+public class BlueInterLineCenter extends BaseAutonomous {
+
+    @Override
+    public void runOpMode() {
+
+        super.runOpMode();
+
+        logger.statusLog(step++, "Ready to run");
+
+        waitForStart();
+
+        logger.statusLog(step++, "Running forward");
+        robot.drivetrain.runDistance(4, 4, 8, 0.2);
+
+        logger.statusLog(step++, "Finished");
+        robot.stopAllMotors();
+
+    }
+
+}
+
